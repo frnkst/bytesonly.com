83fd9d3e39756c265109a7e7fc3c04072d8c930c
Frank <frank@void0.ch>
2022-02-18 22:13:34 +0100

    Use chevron

--- src/components/Post.js
+++ src/components/Post.js
@@ -232,7 +232,10 @@ export default function Post({ meta, children, posts }) {
                 )}
                 <div className="pt-8">
                   <Link href="/">
-                    <a className="text-teal-600 hover:text-teal-700">&larr; Back to the blog</a>
+                    <a className="text-teal-600 hover:text-teal-700 flex">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
+                      </svg>Back to the blog</a>
                   </Link>
                 </div>
               </footer>
